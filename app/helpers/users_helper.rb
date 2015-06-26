c56b6926933ae310449b355b37dce9c6a84bebb9
shohohoh <so10ve.q0fsu0u@gmail.com>
2015-06-26 15:33:32 +0900

    helper修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-  def image_for(user, size = '52')
+  def image_for(user)
     if user.image
-      image_tag "/user_images/#{user.image}", size: size, class: :profile_img
+      image_tag "/user_images/#{user.image}", class: :profile_img
     else
-      image_tag "/01.png", size: size, class: :profile_img
+      image_tag "/01.png", class: :profile_img
     end
   end
 end
