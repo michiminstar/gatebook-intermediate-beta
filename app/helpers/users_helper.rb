f489663933001a9c0199a31b23ab5be41fb69f78
Michi Kanda <michi.kanda.0513@gmail.com>
2015-06-26 11:08:42 +0900

    Modify profile image css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def image_for(user, size = '52')
     if user.image
-      image_tag "/user_images/#{user.image}", size: size, class: :gravatar
+      image_tag "/user_images/#{user.image}", size: size, class: :profile_img
     else
-      image_tag "/01.png", size: size, class: :gravatar
+      image_tag "/01.png", size: size, class: :profile_img
     end
   end
 end
